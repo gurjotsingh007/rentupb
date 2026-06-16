727c6bd7b5f69363adc41e291549f3dfafd855a7
Gurjot Singh <101056456+gurjotsingh007@users.noreply.github.com>
2026-06-16 22:34:11 +0530

    Update new arden 1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect("mongodb+srv://gndu:K5IXNykik3m0kAPV@cluster0.0kfb5u2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {family: 4})
+    mongoose.connect("mongodb+srv://simar1234PCnQPDN9RqtcYOHY@cluster0.irnkbj3.mongodb.net/?appName=Cluster0", {family: 4})
     .then((data) => {
         console.log(`MongoDB connected with server: ${data.connection.host}`);
     })
