35eaa0982f4a1d3f0b839e57b581da584fdd8c51
Gurjot Singh <101056456+gurjotsingh007@users.noreply.github.com>
2026-06-17 18:16:29 +0530

    Update jwtToken.js

--- backend/util/jwtToken.js
+++ backend/util/jwtToken.js
@@ -1,19 +1,16 @@
 const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
-    //option for cookie
     const options = {
-        expires: new Date(
-            Date.now() + 5 * 24 * 60 * 60 * 1000
-        ),
+        expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: true,        // ← ADD THIS
+        sameSite: 'None',    // ← ADD THIS
     };
-
     res.status(statusCode).cookie("token", token, options).json({
         success: true,
         user,
         token
     });
 };
-
 module.exports = sendToken;
