eee51a4e0de272ed737478a309d69115b8505d0b
Gurjot Singh <101056456+gurjotsingh007@users.noreply.github.com>
2026-06-17 18:17:01 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -72,14 +72,16 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.logoutUser = catchAsyncErrors(async(req, res, next) => {
-      res.cookie('token', null, {
+    res.cookie('token', null, {
         expires: new Date(Date.now()),
         httpOnly: true,
-      });
-      res.status(200).json({
+        secure: true,      // ← ADD THIS
+        sameSite: 'None',  // ← ADD THIS
+    });
+    res.status(200).json({
         success: true,
-        message: 'Logged out ',
-      });
+        message: 'Logged out',
+    });
 });
 
 //Get Logged in User Deatil
